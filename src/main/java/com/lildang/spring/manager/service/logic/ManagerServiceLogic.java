44f011b02e1c1bf211ff7ccdc454253908db357f
felixhsm <felixhsm9410@gmail.com>
2025-03-21 14:24:36 +0900

    ADD 마이페이지, 정보수정, 관리자페이지, 신고조회
    마이페이지 본인 데이터가 뜨도록 수정
    정보수정 기능 추가
    관리자페이지에 DB 데이터 출력
    신고조회 들어가도록 수정

--- src/main/java/com/lildang/spring/manager/service/logic/ManagerServiceLogic.java
+++ src/main/java/com/lildang/spring/manager/service/logic/ManagerServiceLogic.java
@@ -1,5 +1,33 @@
 package com.lildang.spring.manager.service.logic;
 
-public class ManagerServiceLogic {
+import java.util.List;
+
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lildang.spring.manager.service.ManagerService;
+import com.lildang.spring.manager.store.ManagerStore;
+import com.lildang.spring.member.domain.MemberVO;
+
+@Service
+public class ManagerServiceLogic implements ManagerService{
+	
+	private ManagerStore mStore;
+	private SqlSession session;
+	@Autowired
+	public ManagerServiceLogic(ManagerStore mStore, SqlSession session) {
+		this.mStore = mStore;
+		this.session = session;
+	}
+	
+	
+	
+	@Override
+	public List<MemberVO> selectList() {
+		List<MemberVO> mList = mStore.selectList(session);
+		return mList;
+	}
 
 }
--- src/main/webapp/WEB-INF/views/member/employee/detail.jsp
+++ src/main/webapp/WEB-INF/views/member/employee/detail.jsp
@@ -21,10 +21,10 @@
 	                <img src="./img/profile.png" alt="profile"> <br>
 	                <button class="imgbtn">사진변경</button>
 	                <div id="information">
-	                    아이디: Ola<br>
-	                    이름: 이용자 <br>
-	                    성별: 여성 <br>
-	                    나이: 25 <br>
+	                    아이디: ${member.id }<br>
+	                    이름: ${member.name } <br>
+	                    성별: ${member.gender } <br>
+	                    나이: ${member.age } <br>
 	                    평점: 4.5/5.0
 	                </div>
 	                <button class="modifybtn">수정하기</button>
